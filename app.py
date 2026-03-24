50a6db2042b1f7853fe763a6d1c79274b5a59f9e
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 11:36:13 +0530

    Add Flask app to generate QR codes from URLs

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_qr():
+    url = request.form['url']
+
+    qr = qrcode.make(url)
+
+    img_io = io.BytesIO()
+    qr.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
